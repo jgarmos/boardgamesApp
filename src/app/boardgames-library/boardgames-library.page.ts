642f65b82abd104b779f25b7872cf37971e5c29f
jgarmos <javiergarciaramos@gmail.com>
2020-04-22 18:07:03 +0200

    added list of games in boardgame-library page

--- src/app/boardgames-library/boardgames-library.page.ts
+++ src/app/boardgames-library/boardgames-library.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Boardgames } from './Boardgames';
+import { BoardgamesApiService } from './../services/boardgames-api.service';
+import { HttpResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-boardgames-library',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardgamesLibraryPage implements OnInit {
 
-  constructor() { }
+  boardgamesList: Boardgames;
+  numberOfGames: number;
 
-  ngOnInit() {
+  constructor(private boargamesAPI: BoardgamesApiService) {
+    this.boardgamesList = new Boardgames();
+    this.numberOfGames = 0;
   }
 
+  ngOnInit() {
+    this.loadBoardgames();
+  }
+
+  loadBoardgames() {
+    this.boargamesAPI.getBoardgames(this.numberOfGames).subscribe(
+      resp_ok => {
+        let httpResponse: HttpResponse<Boardgames> = resp_ok as HttpResponse<Boardgames>;
+        let boardgames = httpResponse.body;
+        this.boardgamesList.games.push(...boardgames.games);
+        console.log("llamando a Api exitosamente");
+        this.boardgamesList.games.map(game => console.log (game.name));
+      }, resp_ko => {
+        console.log("Error al recuperar la lista de películas");
+      }
+    )
+  }
 }
+
